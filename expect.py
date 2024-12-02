800073735f4a9ab4321136fb14a7edc332fd7804
simonfeghali <simonfeghalii@gmail.com>
2024-12-02 20:00:05 +0200

    Update expect.py

--- expect.py
+++ expect.py
@@ -4,13 +4,13 @@ st.title("Life Expectancy Predictor")
 
 # Prompt the user to enter their expected age
 expected_age = st.number_input(
-    "How long do you expect to live? Enter your guess to see what life in Lebanon might say about that!:",
-    min_value=1, step=1
+    "How long do you expect to live? Enter your guess (between 40 and 100):",
+    min_value=40, max_value=100, step=1
 )
 
-# Calculate only if the expected_age is at least 7 to avoid negative results
-if expected_age >= 6:
-    adjusted_age = expected_age - 5
+# Calculate and display only if the expected_age is within the valid range
+if expected_age:
+    adjusted_age = expected_age - 7
     
     # Display the paragraph with the year in larger size
     st.markdown(
@@ -22,5 +22,3 @@ if expected_age >= 6:
         """,
         unsafe_allow_html=True
     )
-else:
-    st.write("Please enter an age of at least 6 to proceed with the calculation.")
